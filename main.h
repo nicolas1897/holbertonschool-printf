0b5c4367222b9eda8167dff96aa54e453da61ea6
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-19 18:17:53 -0500

    created header file main.h also add prototypes, libs and structure

---
+++ main.h
@@ -0,0 +1,30 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/* includ libs */
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ *struct
+ *
+ *
+ *
+ */
+typedef struct ft
+{
+	char format;
+	int (*f)(va_list);
+} formattype;
+
+/* prototype */
+int _printf(const char *format, ...);
+int print_c(va_list arg);
+int print_s(va_list arg);
+int print_mod(va_list arg);
+int print_d(va_list arg);
+int print_i(va_list arg);
+
+#endif /* _MAIN_H_ */
