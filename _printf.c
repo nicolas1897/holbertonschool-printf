bdcfa2d900f46b8261658cf33ff16c58f1e7a869
IngMariale <4725@holbertonsschool.com>
2022-07-20 13:01:26 -0700

    new flow in the printf function

--- _printf.c
+++ _printf.c
@@ -1,36 +1,61 @@
 #include "main.h"
 
-/**
- *
- *
- *
+/**                                                                                                                                     
+
+ *_printf - the information received is printend depending on the parameters                                                            
+
+ *@format: pointer character format                                                                                                     
+
+ *Return: error or number of characteres (cont)                                                                                         
+
  */
+
 int _printf(const char *format, ...)
+  
 {
-	int i = 0, j = 0;
-	va_list arg;
+  
+  int cont;
+  
 
-	formattype format_list[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"%", print_mod},
-		{"d", print_d},
-		{"i", print_i},
-		{NULL, NULL}
-		};
+  
+  formattype format_list[] = {
+			      
+			      {"c", print_c},
+			      
+			      {"s", print_s},
+			      
+			      {"%", print_mod},
+			      
+			      {"d", print_d},
+			      
+			      {"i", print_i},
+			      
+			      {NULL, NULL}
+			      
+  };
+  
 
-	va_start(arg, format);
-	if (format == NULL)
-		return(EXIT_FAILURE);
+  
+  va_list arg;
+  
 
-for (i = 0; format[i] != '\0'; i++)
-{
-	if (format[i] == '%')
-	{
-		j = 0;
-		while (format_list[j].form != 0)
-		{
-		
-		}
-	}
-} 
+  
+  if (format == NULL)
+    
+    {     return(EXIT_FAILURE);
+      
+
+      
+      va_start(arg, format);
+      
+
+      
+      cont = analyzer(format, format_list, arg);
+      
+
+      
+      va_end(arg);
+      
+      return(cont);
+      
+    }
