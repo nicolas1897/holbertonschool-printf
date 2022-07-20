4b65afcf70c48a442af0bec624d16d24989d81fc
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-20 13:50:49 -0500

    use tap in this file _printf.c

--- _printf.c
+++ _printf.c
@@ -7,29 +7,30 @@
  */
 int _printf(const char *format, ...)
 {
-  int i = 0, j = 0;
-va_list arg;
-formattype format_list[] = {
-{"c", print_c},
-{"s", print_s},
-{"%", print_mod},
-{"d", print_d},
-{"i", print_i},
-{NULL, NULL}
-};
-va_start(arg, format);
- if (format == NULL)
-   return(EXIT_FAILURE);
+	int i = 0, j = 0;
+	va_list arg;
 
- for (i = 0; format[i] != '\0'; i++)
-   {
-     if (format[i] == '%')
-       {
-	 j = 0;
-	 while (format_list[j].form != 0)
-	   {
+	formattype format_list[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_mod},
+		{"d", print_d},
+		{"i", print_i},
+		{NULL, NULL}
+		};
 
-	   }
-       }
-   }
- 
+	va_start(arg, format);
+	if (format == NULL)
+		return(EXIT_FAILURE);
+
+for (i = 0; format[i] != '\0'; i++)
+{
+	if (format[i] == '%')
+	{
+		j = 0;
+		while (format_list[j].form != 0)
+		{
+		
+		}
+	}
+} 
