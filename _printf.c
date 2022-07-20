ea13d10f36ecc93e728b33d3975d23debc5a8c27
IngMariale <4725@holbertonsschool.com>
2022-07-20 10:34:16 -0700

    creation of the function inside printf

--- _printf.c
+++ _printf.c
@@ -7,6 +7,29 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+  int i = 0, j = 0;
+va_list arg;
+formattype format_list[] = {
+{"c", print_c},
+{"s", print_s},
+{"%", print_mod},
+{"d", print_d},
+{"i", print_i},
+{NULL, NULL}
+};
+va_start(arg, format);
+ if (format == NULL)
+   return(EXIT_FAILURE);
 
-	va_start arg;
+ for (i = 0; format[i] != '\0'; i++)
+   {
+     if (format[i] == '%')
+       {
+	 j = 0;
+	 while (format_list[j].form != 0)
+	   {
+
+	   }
+       }
+   }
+ 
