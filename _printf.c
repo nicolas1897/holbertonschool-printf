cebf3d9cf16016501cae2bb5e6f9579bab6af58c
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-20 19:50:25 -0500

    deleted withe space _printf.c

--- _printf.c
+++ _printf.c
@@ -1,61 +1,29 @@
 #include "main.h"
 
-/**                                                                                                                                     
-
- *_printf - the information received is printend depending on the parameters                                                            
-
- *@format: pointer character format                                                                                                     
-
- *Return: error or number of characteres (cont)                                                                                         
-
+/**
+ *_printf - the information received is printend depending on the parameters
+ *@format: pointer character format
+ *Return: error or number of characteres (cont)
  */
-
 int _printf(const char *format, ...)
-  
 {
-  
-  int cont;
-  
+	int cont = 0;
+	va_list arg;
 
+	formattype format_list[] = {
+		{"c", print_c},
+		{"s", print_s},     
+		{"%", print_mod},
+		{"d", print_d},
+		{"i", print_i},
+		{NULL, NULL}   
+		};
   
-  formattype format_list[] = {
-			      
-			      {"c", print_c},
-			      
-			      {"s", print_s},
-			      
-			      {"%", print_mod},
-			      
-			      {"d", print_d},
-			      
-			      {"i", print_i},
-			      
-			      {NULL, NULL}
-			      
-  };
-  
+if (format == NULL)    
+	return(EXIT_FAILURE);
 
-  
-  va_list arg;
-  
-
-  
-  if (format == NULL)
-    
-    {     return(EXIT_FAILURE);
-      
-
-      
       va_start(arg, format);
-      
-
-      
       cont = analyzer(format, format_list, arg);
-      
-
-      
       va_end(arg);
-      
       return(cont);
-      
-    }
+}
