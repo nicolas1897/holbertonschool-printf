48cae25e4f70a76e4583b0442a3e0df3816286c1
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-21 11:38:33 -0500

    created print string function

--- print_s.c
+++ print_s.c
@@ -8,6 +8,15 @@
  */
 int print_s(va_list arg)
 {
-        _putchar(va_arg(arg, int));
-        return (1);
+	int i = 0;
+	char *str;
+
+	str = va_arg(arg, char*);
+	
+	for (i = 0 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+
+	if (str == NULL)
+		str = "(null)";
+	return (i);
 }
