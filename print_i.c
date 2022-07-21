bed3ea79ce41e3782897f1c6cec8fe5d0c31cd41
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-20 20:48:02 -0500

    add fucntions

---
+++ print_i.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *print_c - print character
+ *@arg: argument list
+ *
+ *Return: always 1
+ */
+int print_i(va_list arg)
+{
+	_putchar(va_arg(arg, int));
+	return (1);
+}
