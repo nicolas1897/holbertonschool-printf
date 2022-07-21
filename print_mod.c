5727c2a5e878e17dbe3c49babde2c4eba4524cf6
Luiyi_FHB <luiyifernando@gmail.com>
2022-07-21 12:14:21 -0500

    fixed prototype prin_mod.c to print_mod

--- print_mod.c
+++ print_mod.c
@@ -1,12 +1,11 @@
 #include "main.h"
  
  /**
- *
- *
- *
- *
+ *print_mod - print percetage symbol
+ *@arg: agurment list
+ *Return: always 1
  */
-int print_mod.c(__attribute__((unused))va_list arg)
+int print_mod(__attribute__((unused))va_list arg)
 {
   _putchar('%');
   return (1);
